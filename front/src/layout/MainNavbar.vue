00df8a942e4aefbb90cf9f5daebaa1f13caafb80
Breno Sá <breno.almeida.sa@gmail.com>
2022-05-21 11:35:18 -0300

    design(navbar): design do navbar

--- front/src/layout/MainNavbar.vue
+++ front/src/layout/MainNavbar.vue
@@ -1,21 +1,34 @@
 <template>
   <md-toolbar id="toolbar" md-elevation="0" class="md-absolute" :class="extraNavClasses">
-    <div class="md-toolbar-row md-collapse-lateral">
-      <div class="md-toolbar-section-start">
-        <h3 class="md-title">Fast Coffee</h3>
-      </div>
+  <img src="../assets/img/logo_fast_coffee.png"  alt="Fast Coffee" width="80">
+    <div class="md-toolbar-row md-collapse-lateral">   
       <div class="md-toolbar-section-end">
         <div class="md-collapse">
           <div class="md-collapse-wrapper">
             <md-list>
               <md-list-item href="#">
-                <i class="material-icons">content_paste</i>
-                <p>Documentation</p>
+                <i class="material-icons">menu_book</i>
+                <p>Cardápio</p>
               </md-list-item>
 
               <md-list-item href="#">
-                <i class="material-icons">cloud_download</i>
-                <p>Download</p>
+                <i class="material-icons">new_releases</i>
+                <p>Novidades</p>
+              </md-list-item>
+
+               <md-list-item href="#">
+                <i class="material-icons">attach_money</i>
+                <p>Promoções</p>
+              </md-list-item>
+
+               <md-list-item href="#">
+                <i class="material-icons">shopping_cart</i>
+                <p>Meu Pedido</p>
+              </md-list-item>
+
+               <md-list-item href="#">
+                <i class="material-icons">help</i>
+                <p>Solicitar Ajuda</p>
               </md-list-item>
             </md-list>
           </div>
