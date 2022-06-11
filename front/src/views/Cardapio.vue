9ab01417c56ed69ce52776b57d513eb9c7b71306
Breno Sá <breno.almeida.sa@gmail.com>
2022-06-11 16:11:16 -0300

    feat(front): criação da página de cardápio

--- front/src/views/Cardapio.vue
+++ front/src/views/Cardapio.vue
@@ -5,23 +5,30 @@
     <div class="main main-raised">
       <div class="section">
         <div class="container">
-          <div class="item" v-for="produto in cardapio" :key="produto.id">
+          <div class="item" v-for="product in menu" :key="product.id">
             <div class="features ">
               <div class="md-layout">
-                <div class="md-layout-item md-medium-size-10 md-small-size-100">
-                  <img :src="produto.caminhoImagem" :alt="produto.caminhoImagem">
+                <div class="md-layout-item md-size-10">
+                  <img :src="product.imageLocation" :alt="product.imageLocation">
                 </div>
-                <div class="md-layout-item md-medium-size-80 md-small-size-100">
-                  <h3>{{ produto.nome }}</h3>
-                  <span>{{ produto.descricao }}</span>
+                <div class="md-layout-item md-size-60">
+                  <h3>{{ product.name }}</h3>
+                  <span>{{ product.description }}</span>
                 </div>
-                <div class="md-layout-item md-medium-size-10 md-small-size-100">
-                  qtd
+                <div class="md-layout-item md-size-10 center input">
+                  <input class="vertical-center" :id="product.id" type="number" v-model="product.quantity"
+                    placeholder="0" min="0" />
+                </div>
+                <div class="md-layout-item md-size-20 center">
+                  <h4><strong>Valor unitário</strong></h4>
+                  <span class="cost">{{ product.cost }}</span>
+                  <h4><strong>Total</strong></h4>
+                  <span class="cost">{{ (product.cost * (product.quantity) || "-") }}</span>
                 </div>
               </div>
             </div>
           </div>
-          <md-button slot="buttons" href="javascript:void(0)" class="botao-finalizar">
+          <md-button slot="buttons" href="javascript:void(0)" class="botao-finalizar" @click="getOrder">
             Finalizar pedido
           </md-button>
         </div>
@@ -36,35 +43,36 @@ import axios from "axios";
 export default {
   data() {
     return {
-      cardapio: [{
-        id: 1,
-        nome: "Espresso",
-        descricao: "Extração intensa dos atributos sensoriais do café. A moagem fina unida a pressão da água traz um bebida intensa...  Leia Mais ->",
-        caminhoImagem: "img",
-        categoria: "espresso"
-      }, {
-        id: 2,
-        nome: "Panna",
-        descricao: "Panna quer dizer 'creme' em italiano. Uma deliciosa cobertura de chantilly adicionada ao espresso...  Leia Mais ->",
-        caminhoImagem: "img",
-        categoria: "espresso"
-      }]
+      menu: [],
+      order: []
     }
   },
   created() {
-    this.getProduct();
+    this.getMenu();
   },
   methods: {
-    getProduct() {
-      console.log(process.env.VUE_APP_SERVER_URL)
+    getMenu() {
+      console.log('process.env.VUE_APP_SERVER_URL', process.env.VUE_APP_SERVER_URL)
       axios
-        .get(process.env.VUE_APP_SERVER_URL + "product")
+        .get(process.env.VUE_APP_SERVER_URL + "menu")
         .then((res) => {
-          this.cardapio = res.data.cardapio;      
+          console.log('res', res.data)
+          this.menu = res.data;
         })
         .catch((error) => {
           console.log(error);
         });
+    },
+    getOrder() {
+      this.menu.forEach(item => {
+        if (item.quantity) {
+          this.order.push({
+            ...item,
+            totalCost: parseInt(item.quantity) * parseFloat(item.cost)
+          })
+        }
+      });
+      this.$router.push({ name: 'carrinho',  params: this.order })
     }
   }
 }
@@ -72,8 +80,9 @@ export default {
 
 <style lang="scss" scoped>
 h3,
-span {
-  color: white
+span.cost {
+  color: white;
+  font-weight: bold
 }
 
 .item {
@@ -91,5 +100,25 @@ span {
 
 .botao-finalizar:hover {
   background-color: rgba(129, 0, 0, 0.8) !important;
+}
+
+input[type='number'] {
+  width: 60px;
+}
+
+.center {
+  text-align: center;
+}
+
+.vertical-center {
+  display: inline-block;
+  vertical-align: middle;
+  line-height: normal;
+}
+
+div.input {
+  height: 150px;
+  line-height: 150px;
+  text-align: center;
 }
 </style>
